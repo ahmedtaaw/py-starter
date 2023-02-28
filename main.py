b80721803a0a1b6621f61ffd6ffdd83c8998541c
Ahmed Tawfik <u087775@lab.saab.ae>
2023-02-28 14:52:56 +0400

    square feet

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ with_name = greetings.format(name,"today")
 with_name2 = greetings.format(name,"Monday")
 print(with_name)
 print(with_name2)
+
+
+size_input = input("How big is your house");
+square_feet = int(size_input);
+square_metres = square_feet/10.8;
+
+print(f"{square_feet} square feet is {square_metres:.2f} square metres.")
